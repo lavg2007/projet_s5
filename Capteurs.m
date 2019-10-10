ea56b1b6975830306537df2a896c1944ff7d1683
forj1928 <jacob.fortin2@usherbrooke.ca>
2019-10-10 11:49:33 -0400

    Ajout de la modelisation du capteurè

---
+++ Capteurs.m
@@ -0,0 +1,28 @@
+%% mesures avec -1A
+
+load('capteur.mat')
+ 
+d1 = distance;
+d0 = d1.^0;
+d2 = d1.^2;
+d3 = d1.^3;
+d4 = d1.^4;
+
+Y1 = voltage;
+X1 = [d0 d1 d2 d3 d4];
+
+a = pinv(X1)*Y1;
+a0 = a(1);
+a1 = a(2);
+a2 = a(3);
+a3 = a(4);
+a4 = a(5);
+
+V = a0*d0 + a1*d1 + a2*d2 + a3*d3 + a4*d4;
+
+figure(1)
+hold on
+plot(distance,V)
+plot(distance,voltage)
+legend('Simulation selon les valeurs calculees','Simulation selon les valeurs mesurees')
+hold off
