b2b69c4b93f89c7c8818274ef24a18d5ebcfce3d
sharingammi <48485740+sharingammi@users.noreply.github.com>
2019-12-03 01:08:35 -0500

    Commentaires ajoutés
    CompensateurPlaque.m
    DoubleAvanceDePhase.m
    ProportionnelIntegralV1.m
    AvancePhaseBode1.m

--- Functions/ProportionnelIntegralV1.m
+++ Functions/ProportionnelIntegralV1.m
@@ -1,3 +1,28 @@
+%**************************************************************************
+% 
+% Fichier: ProportionnelIntegralV1.m
+% Date: 2019-12-02
+% Université de Sherbrooke - Génie électrique
+% 
+% Équipe P4:
+%           Brittany Latour - latb2901 
+%           Samuel Mathieu - mats2510 
+%           Jacob Fortin - forj1928 
+%           Gabriel Lavoie - lavg2007 
+%           Olivier Chrétien-Rioux - chro2901 
+%           Sarah Clauzade - clas2902 
+%           Thierry Letalnet - lett2101 
+%
+% Résumé: Calcul d'un compensateur proportionnel intégral (PI)
+% 
+% Entrés:  FTBO -> Une fonction de transfert en boucle ouverte
+%          s -> un angle (en degrée)
+%          marge -> valeur numérique de la marge de phase désiré
+% 
+% Sortie:  Gpi -> Fonction de transfert du compensateur PI
+% 
+%**************************************************************************
+
 function [ Gpi ] = ProportionnelIntegralV1( FTBO,s,marge )
 %Compensateur PI version simple
 %   [ Gpi ] = ProportionnelIntegralV1( num,den,s )
